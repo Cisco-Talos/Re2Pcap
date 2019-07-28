81e0b11842d56b0fa03de72c2a391417c143e8c0
Amit Raut <amiraut@cisco.com>
2019-07-28 16:30:56 -0400

    Implemented simulated Request/Response for GET, POST, and HEAD method

--- Re2Pcap/Re2Pcap.py
+++ Re2Pcap/Re2Pcap.py
@@ -24,7 +24,12 @@
 # Flask based web UI wrapper for Re2Pcap
 from flask import render_template, Flask, request, url_for, redirect, send_file
 from datetime import datetime
-import os, subprocess, time, socket
+import os, subprocess, time, socket, httpretty
+from flask import jsonify
+
+from io import BytesIO, StringIO
+
+from http.client import HTTPResponse
 
 app = Flask("Re2Pcap")
 
@@ -32,37 +37,111 @@ app = Flask("Re2Pcap")
 def index():
     return render_template('index.html')
 
+
+# Fake socket class for http response parsing
+class FakeSocket():
+	def __init__(self, response_str):
+		self._file = BytesIO(response_str)
+	def makefile(self, *args, **kwargs):
+		return self._file
+
+
+# Function to parse HTTP Response and return response object
+def parse_Response(res_file):
+	try:
+		response_str = res_file
+		source = FakeSocket(response_str.encode('latin-1'))
+		response = HTTPResponse(source)
+		response.begin()
+		# response_body = response.read()
+	except Exception as e:
+		return {"error": "Failed to Parse Given HTTP Raw Response. Please Verify your Input and Try Again."}
+	return response
+
+
+# Function to parse HTTP Request and return (req, port, postParam)
+def parse_Request(req_file):
+	try:
+		# Parse the HTTP raw request in terms of headers, URI, client body
+		req = httpretty.core.HTTPrettyRequest(req_file)
+		if req.error_code == 400:
+			logger.error(req.error_message)
+		else:
+			# Get POST parameter values. Httpretty parsing of post param adds `\n\r\n\r` so getting rid of last 4 bytes
+			postParam = str(req.rfile.read(), 'utf-8')[:-2]
+			if not req.headers['host']:
+				logger.error(' Please Add Host Request Header')
+			elif ':' in req.headers['host']:
+				port = int(req.headers['host'][req.headers['host'].index(':') + 1:])
+			else:
+				port = 80
+	except Exception as e:
+		return {"error": "Failed to Parse Given HTTP Raw Request. Please Verify your Input and Try Again."}
+	return req, port, postParam
+
+
+# @app.route('/validate', methods=["POST"])
+def validate(inputRequest, inputResponse):
+	# inputRequest = request.form['inputRequest']
+	# inputResponse = request.form['inputResponse']
+	
+	# Try to parse the input Request and Response and respond via Ajax before processing input via Re2Pcap script
+	is_request_parsable = parse_Request(inputRequest)
+	is_response_parsable = parse_Response(inputResponse)
+	errorDict = {}
+	command = ""
+
+	if (len(inputRequest) and isinstance(is_request_parsable, dict)) or (len(inputResponse) and isinstance(is_response_parsable, dict)) or (not len(inputRequest) and not len(inputResponse)):
+		return False
+	else:
+		return True
+
+
 @app.route('/createPcap', methods=["POST"])
 def createPcap():
-    inputText = request.form['inputText']
-    # if request.form['requestType'] == 'request':
-    if len(inputText):
-        # Write request from the textarea to file
-        reqFile = os.path.join(os.getcwd(), 'io/', str(datetime.now()).replace(' ', '-') + '.req')
-        f = open(reqFile, 'w+')
-        f.write(inputText)
-        f.close()
-        # Create command to be passed to the req2pcap
-        command = os.path.join(os.getcwd(), 'Re2Pcap') + ' ' + reqFile
-    else:
-        command = 'fail'
-    try:
-        if len(command) > 5:
-            childProc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-            time.sleep(5)
-            childProc.communicate()
-            if not childProc.returncode:
-                resultFileName = request.form['pcapFileName'] + '.pcap' if request.form[
-                    'pcapFileName'] else 'Re2Pcap-Result.pcap'
-                return send_file(reqFile + '-result.pcap', as_attachment=True, attachment_filename=resultFileName)
-            else:
-                return '<h3>Something went Wrong :( Please Check the Input/ Error log and Try Again ....</h3>'
-        else:
-            return '<h3>Something went Wrong :( Please Check the Input/ Error log and Try Again ....</h3>'
-    except subprocess.CalledProcessError:
-        return '<h3>Something went Wrong :( Please Check the Input/ Error log and Try Again ....</h3>'
+	inputRequest = request.form['inputRequest']
+	inputResponse = request.form['inputResponse']
+
+	if not validate(inputRequest, inputResponse):
+		return jsonify(error="Failed to Parse Input Request or Response. Please Verify your Input and Try Again....")
+		
+
+	if len(inputRequest):
+	# Write request from the textarea to file
+		reqFile = os.path.join(os.getcwd(), 'io/Request.req')
+		f = open(reqFile, 'w+')
+		f.write(inputRequest)
+		f.close()
+		command = os.path.join(os.getcwd(), 'Re2Pcap') + ' ' + reqFile
+	else:
+		command = os.path.join(os.getcwd(), 'Re2Pcap') + ' ' + "NO"
+
+	if len(inputResponse):
+		# Write Response from the textarea to file
+		resFile = os.path.join(os.getcwd(), 'io/Response.res')
+		f = open(resFile, 'w+')
+		f.write(inputResponse)
+		f.close()
+		command += ' ' + resFile
+	else:
+		command += ' ' + "NO"
+	
+	try:
+		if len(command) > 5:
+			childProc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+			time.sleep(5)
+			childProc.communicate()
+			if not childProc.returncode:
+				resultFileName = request.form['pcapFileName'] + '.pcap' if request.form['pcapFileName'] else 'Re2Pcap-Result.pcap'
+				return send_file('io/Re2Pcap-result.pcap', as_attachment=True, attachment_filename=resultFileName)
+			else:
+				return jsonify(error='Something went Wrong :( Please Check the Input/ Error log and Try Again ....')
+		else:
+			return jsonify(error='Something went Wrong :( Please Check the Input/ Error log and Try Again ....')
+	except subprocess.CalledProcessError:
+		return jsonify(error='Something went Wrong :( Please Check the Input/ Error log and Try Again ....')
 
 
 if __name__ == '__main__':
-    host_ip = socket.gethostbyname(socket.gethostname())
-    app.run(host=os.getenv('IP', host_ip), port=int(os.getenv('PORT', 5000)), debug=True)
+	host_ip = socket.gethostbyname(socket.gethostname())
+	app.run(host=os.getenv('IP', host_ip), port=int(os.getenv('PORT', 5000)), debug=True)
--- Re2Pcap/static/js/myscript.js
+++ Re2Pcap/static/js/myscript.js
@@ -1,22 +1,29 @@
-var placeholder = 'POST /index.html HTTP/1.1\nHost: 10.10.10.1:8080\nUser-Agent: Mozilla\n\nHello World! \n\nOR\n\nHTTP/1.1 200 OK\nServer: nginx\n\nHello World! ';
-$('#inputText').attr('placeholder', placeholder);
+var requestPlaceholder = 'POST /index.html HTTP/1.1\nHost: 10.10.10.1:8080\nUser-Agent: Mozilla/5.0\n\nHello World!\n';
+$('#inputRequest').attr('placeholder', requestPlaceholder);
 
-$('#submit').prop('disabled', true);
-$('#inputText').on({
-    'focusout': handleInput,
-    'input': handleInput,
-    'focus': handleInput,
-    'keyup': handleInput
+var responsePlaceholder = 'HTTP/1.1 200 OK\nServer: nginx\nContent-Length: 13\n\nHello World!\n';
+$('#inputResponse').attr('placeholder', responsePlaceholder);
+
+$(document).ready(function() {
+	$('#Re2Pcap').on('submit', function(event) {
+		$.ajax({
+			data: {
+				in_Request : $('#inputRequest').val(),
+				in_Response : $('#inputResponse').val()
+			},
+			type: 'POST',
+			url: '/validate'
+		})
+		.done(function(data) {
+			if (data.error) {
+				$('#errorAlert').text(data.error).show();
+				$('#successAlert').hide();
+			}
+			else {
+                $('#successAlert').text("Parsed the Input Correctly. Creating PCAP; Please wait....").show();
+				$('#errorAlert').hide();
+			}
+		});
+		event.preventDefault();
+	});
 });
-
-function handleInput() {
-    var text = $('#inputText').val().split('\n')[0];
-    var re = /^(HTTP\/\d{1}\.\d{1}|GET|POST|HEAD|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH)\ .*$/;
-    var is_valid = re.test(text);
-    if(is_valid) {
-        $('#submit').prop('disabled', false);
-    }
-    else {
-        $('#submit').prop('disabled', true);
-    }
-}
